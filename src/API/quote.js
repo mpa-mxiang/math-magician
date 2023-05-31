47c288b04025d36cd6a7291a9c2eb8e556b6e189
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-05-30 22:47:06 -0400

    display quote.js success

--- src/API/quote.js
+++ src/API/quote.js
@@ -1,5 +1,43 @@
-export const getScore = async () => {
-    const response = await fetch('https://api.api-ninjas.com/v1/quotes?category=computers');
-    const results = await response.json();
-    return results.result;
-  };
+import React, { useState, useEffect } from 'react';
+
+const Quote = () => {
+  const [quote, setQuote] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchQuote = async () => {
+      try {
+        const response = await fetch('https://api.api-ninjas.com/v1/quotes?category=computers');
+        if (!response.ok) {
+          throw new Error('Failed to fetch quote');
+        }
+        const data = await response.json();
+        setQuote(data.quote);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error.message);
+        setIsLoading(false);
+      }
+    };
+
+    fetchQuote();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return (
+      <div>
+        Error:
+        {error}
+      </div>
+    );
+  }
+
+  return <div className="quote">{quote}</div>;
+};
+
+export default Quote;
