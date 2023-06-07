9b29dec34295e3d81c3a32c7ca2035791d6784e9
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-06-07 19:00:11 -0400

    add three more test cases for calculate

--- src/logic/calculate.test.js
+++ src/logic/calculate.test.js
@@ -16,4 +16,47 @@ describe('Calculator', () => {
       operation: null,
     });
   });
+  it('should clear the calculator data when 0 button is clicked', () => {
+    const obj = {
+      total: '100',
+      next: '0',
+      operation: '+',
+    };
+
+    const result = calculate(obj, '0');
+
+    expect(result).toEqual({});
+  });
+
+  it('should give the calculate result when operation button is clicked', () => {
+    const obj = {
+      total: '100',
+      next: '1.1',
+      operation: '+',
+    };
+
+    const result = calculate(obj, '+');
+
+    expect(result).toEqual({
+      total: '101.1',
+      next: null,
+      operation: '+',
+    });
+  });
+
+  it('should give the reverse number when +/- button is clicked', () => {
+    const obj = {
+      total: '100',
+      next: '10',
+      operation: '+/-',
+    };
+
+    const result = calculate(obj, '+/-');
+
+    expect(result).toEqual({
+      total: '100',
+      next: '-10',
+      operation: '+/-',
+    });
+  });
 });
